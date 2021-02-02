06077391be79b89709be4e477018a3930a948e89
Aditya Thati <aditya.thati@gmail.com>
2021-02-02 10:19:36 +0530

    Modified path to pdftotext

--- QuoteEngine/PDFIngestor.py
+++ QuoteEngine/PDFIngestor.py
@@ -11,7 +11,7 @@ class PDFIngestor(IngestorInterface):
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
         txt = './pdfInText.txt'
-        cmd = r"""{} "{}" "{}" """.format("pdftotext_Linux -layout -nopgbrk", path, txt) 
+        cmd = r"""{} "{}" "{}" """.format("./pdftotext_Linux -layout -nopgbrk", path, txt) 
         subprocess.call(cmd, shell=True, stderr=subprocess.STDOUT)
         quotes = TxtIngestor.parse(txt)
         os.remove(txt)
